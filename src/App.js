cded1e9e03c8b924ad2bba632ece60722fe096be
Nicolas <casteranicolas.contact@gmail.com>
2022-06-21 14:13:59 +0200

    Add global files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage";
+import "./styles/global.scss"
+
 function App() {
   return (
     <div className="App">
+      <BrowserRouter >
+        <Routes>
+          <Route path="/accueil" element={<HomePage />}/>
+          <Route path="/" element={<HomePage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/homePage/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './_homePage.scss'
+
+const HomePage = () => {
+    return (
+        <>
+            <header className='homePageHeader'></header>
+            <main className='homePageMain'>
+                <h1>Hello Portfolio !</h1>
+            </main>
+        </>
+    );
+};
+
+export default HomePage;
