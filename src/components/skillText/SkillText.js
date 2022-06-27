4c6df1bfc5c20dcc210e40f5f21e80d474de3e0f
Nicolas <casteranicolas.contact@gmail.com>
2022-06-27 10:18:56 +0200

    Update SkillText.js

--- src/components/skillText/SkillText.js
+++ src/components/skillText/SkillText.js
@@ -8,18 +8,18 @@ const SkillText = (props) => {
             <p>
                 <span className='top'>
                     It is a long established fact that a reader will be distracted by the
-                    readable content of a page when lookingat its layout. The point of using
+                    readable content <span className='jsText'>Js</span> a page when lookingat its layout. The point of using
                     Lorem Ipsum is that it has a more-or-less normal distribution of letters, 
                     as opposed to using 'Content here.
-                    'lorem ipsum' will uncover many web sites still in their infancy. Various 
+                    'lorem ipsum' will uncover many web sites still in <span className='gsapText'>Gsap</span> infancy. 
                 </span>
                 <span className='middle'>
                     Now use Many desktop public <span className='reactText'>react</span> packages and web page editors 
                 </span>
                 <span className='bottom'>
-                    It is a long established fact that a reader will be distracted by the
+                    It is a long established fact that a <span className='jestText'>jest</span> will be distracted by the
                     now use Lorem Ipsum as their default model text, and a search for
-                    'lorem ipsum' will uncover many web sites still in their infancy. Various 
+                    'lorem ipsum' will uncover many web sites still in <span className='sassText'>Sass</span> infancy. Various 
                     versions have evolved over the years, sometimes by accident, sometimes on 
                     purpose.
                 </span>
