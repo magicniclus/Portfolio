28ab2bba640b1d864eab7b29a03ea8a8d733662d
Nicolas <casteranicolas.contact@gmail.com>
2022-06-28 10:57:53 +0200

    Update AllProjects.js
    Add dateContainer

--- src/components/allProjects/AllProjects.js
+++ src/components/allProjects/AllProjects.js
@@ -7,19 +7,47 @@ const AllProjects = () => {
             <div className="projects">
                 <div className="projectContainer one">
                     <div className="imgContainer"></div>
-                    <div className="description"></div>
+                    <div className="descriptionContainer">
+                        <div className="description">
+                            <div className="bottomContainer">
+                                <p>1</p>
+                                <p className='date'>2022</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="projectContainer two">
                     <div className="imgContainer"></div>
-                    <div className="description"></div>
+                    <div className="descriptionContainer">
+                        <div className="description">
+                            <div className="bottomContainer">
+                                <p className='number'>2</p>
+                                <p className='date'>2022</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="projectContainer terabois">
                     <div className="imgContainer"></div>
-                    <div className="description"></div>
+                    <div className="descriptionContainer">
+                        <div className="description">
+                            <div className="bottomContainer">
+                                <p>3</p>
+                                <p className='date'>2022</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="projectContainer four">
                     <div className="imgContainer"></div>
-                    <div className="description"></div>
+                    <div className="descriptionContainer">
+                        <div className="description">
+                            <div className="bottomContainer">
+                                <p>4</p>
+                                <p className='date'>2022</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <div className="title">
