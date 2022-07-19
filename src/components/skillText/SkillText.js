0064af165ff3cbb9e5c632e9a4cc4ef77b9c6983
Nicolas <casteranicolas.contact@gmail.com>
2022-07-19 11:34:02 +0200

    Update SkillText.js

--- src/components/skillText/SkillText.js
+++ src/components/skillText/SkillText.js
@@ -25,19 +25,18 @@ const SkillText = (props) => {
     return (
         <div className={show ? "skillTextContainer" : "skillTextContainer hidden"}>
             <div className='lignContainer'>
-                    <p className='lign lignOne'>It is a long established fact that a reader will be distracted <span className={light === "js" ? 'jsText show' : "jsText"}>Js</span> a page </p>
-                    <p className="lign lignTwo">when lookingat its layout. The point of using</p> 
-                    <p className="lign lignThree">Lorem Ipsum is that it has a more-or-less normal distribution of letters, </p>
-                    <p className="lign lignFour">as opposed to using 'Content here.</p>
-                    <p className="lign lignFive">'lorem ipsum' will uncover many web sites still in <span className={light === "gsap" ? 'gsapText show' : "gsapText"}>Gsap</span> infancy. </p>
-                    <p className="lign lignSix">Now use Many desktop public <span className={light === "react" ? 'reactText show' : "reactText"}>react</span> packages and web page editors </p> 
-                    <p className="lign lignSeven">It is a long established fact that a <span className={light === "jest" ? 'jestText show' : "jestText"}>jest</span> will be distracted by the</p>
-                    <p className="lign lignEight">now use Lorem Ipsum as their default model text, and a search for</p>
-                    <p className="lign lignNine">'lorem ipsum' will uncover many web sites still in <span className={light === "sass" ? 'sassText show' : "sassText"}>Sass</span> infancy. Various </p>
+                    <p className='lign lignOne'>It is a long established fact that a reader will be distracted <span className={light === "js" ? 'jsText show' : "jsText"}>Js</span></p>
+                    <p className="lign lignTwo">when lookingat its layout. The point of using more-or-less sites</p> 
+                    <p className="lign lignThree">Lorem Ipsum is that it has a more-or-less normal distribution, </p>
+                    <p className="lign lignFour">as opposed to using 'Conten tweb sites still in here sometimes.</p>
+                    <p className="lign lignFive">'lorem ipsum' will uncover many sites still in Ipsum <span className={light === "gsap" ? 'gsapText show' : "gsapText"}>Gsap</span> infancy. </p>
+                    <p className="lign lignSix">Now use Many desktop <span className={light === "react" ? 'reactText show' : "reactText"}>react</span> packages and web page editors </p> 
+                    <p className="lign lignSeven">It is a long established sometimes by accident<span className={light === "jest" ? 'jestText show' : "jestText"}>jest</span> will be distracted.</p>
+                    <p className="lign lignEight">Now use Lorem Ipsum as their default model text, and a search for</p>
+                    <p className="lign lignNine">'lorem ipsum' will still in The point of using<span className={light === "sass" ? 'sassText show' : "sassText"}>Sass</span> infancy. Various </p>
                     <p className="lign lignTen">versions have evolved over the years, sometimes by accident</p>
-                    <p className="lign lignEleven">purpose.</p>
             </div>
-            <img src=".../.../" alt="" />
+            <div className="imgContainer"></div>
         </div>
     );
 };
