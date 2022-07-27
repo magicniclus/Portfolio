d9224e7c7718208b8397feef77fb49a159ec909a
Nicolas <casteranicolas.contact@gmail.com>
2022-07-27 20:29:30 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
 import "./styles/global.scss"
 
 function App() {
+
   return (
     <div className="app">
       <BrowserRouter >
         <Routes>
-          {
-
-          }
           <Route path="/accueil" element={<HomePage />}/>
           <Route path="/" element={<HomePage />}/>
         </Routes>
