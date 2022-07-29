2243284eabc05e540a7554bee7f1f682af8d7f79
Nicolas <casteranicolas.contact@gmail.com>
2022-07-29 11:11:24 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import HomePage from "./pages/homePage/HomePage";
+import TestPage from "./pages/testPage/TestPage";
 import "./styles/global.scss"
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/accueil" element={<HomePage />}/>
           <Route path="/" element={<HomePage />}/>
+          <Route path="/test" element={<TestPage />}/>
         </Routes>
       </BrowserRouter>
     </div>
