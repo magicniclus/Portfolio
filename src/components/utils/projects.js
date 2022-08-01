b3441143bade4ec4172167a2bd60975fc35f6164
Nicolas <casteranicolas.contact@gmail.com>
2022-08-01 14:02:07 +0200

    Create projects.js

---
+++ src/components/utils/projects.js
@@ -0,0 +1,38 @@
+const projects = {
+    terabois : {
+        id: 1,
+        title: "terabois",
+        city: "bordeaux",
+        years: "2022",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
+        img: "one.jpg"
+    },
+    maisonsur : {
+        id: 2,
+        title: "maison-sûr",
+        city: "Paris",
+        years: "2017",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
+        img: "two.jpg"
+    },
+    magma : {
+        id: 2,
+        title: "magma",
+        city: "Paris",
+        years: "2019",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
+        img: "three.jpg"
+    },
+    threejs : {
+        id: 2,
+        title: "three-js",
+        city: "Bordeaux",
+        years: "2020",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
+        img: "four.jpg"
+    }
+}
