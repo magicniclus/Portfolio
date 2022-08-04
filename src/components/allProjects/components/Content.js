43618487febc1776a2f0038aaaad8fee4114421f
Nicolas <casteranicolas.contact@gmail.com>
2022-08-04 14:15:52 +0200

    Update Content.js

--- src/components/allProjects/components/Content.js
+++ src/components/allProjects/components/Content.js
@@ -6,13 +6,14 @@ import { gsap } from "gsap";
 const Content = (props) => {
     //Gsap
     const tl = useRef()
-
+    
     const refCity = useRef(null)
     const refParams = useRef(null)
-
+    
     const lineRefs = useRef([]);
     lineRefs.current = []
 
+    /* Creating a timeline that will animate the city and params. */
     useEffect(()=>{
         tl.current = gsap.timeline({
             default:{
@@ -24,24 +25,54 @@ const Content = (props) => {
         .fromTo(refParams.current, {x: -500}, {x:0},"-=0.1")
     },[])
 
+    /* Creating a timeline that will animate the letters of the title. */
     useEffect(()=>{
-        const timeline = gsap.timeline({
-            default:{
-                ease: "Power4.inOut"
-            }
-        })
-        lineRefs.current.map((el, i )=> {
-            if(i === 0){
-                timeline.fromTo(el, {y: 300}, {autoAlpha:1, y:0, duration: 0.5, ease: "Power4.inOut", delay: 0.5})
-            }else{
-                timeline.fromTo(el, {y: 300}, {autoAlpha:1, y:0, duration: 0.5, ease: "Power4.inOut"},"-=0.4")
-            }
-        })
+        updateLineRefs()
     }, [])
-
+    
+    const updateLineRefs = () => {
+        setTimeout(()=>{
+            const timeline = gsap.timeline({
+                default:{
+                    ease: "Power4.inOut"
+                }
+            })
+            lineRefs.current.map((el, i )=> {
+                if(i === 0){
+                    timeline.fromTo(el, {opacity:1, y: 300}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut", delay: 0.5})
+                }else{
+                    timeline.fromTo(el, {opacity:1, y: 300}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut"},"-=0.16")
+                }
+            })
+        }, 100)
+    }
+    
     //component
     const content = props.content;
     const blockActive = props.active
+    
+    const [oldContent, setOldContent] = useState(null)
+    const [newContent, setNewContent] = useState(null)
+
+    const [counter, setCounter] = useState(0)
+
+    
+    /**
+     * It takes an element as an argument and adds it to the lineRefs array if it's not already in
+     * there
+     * @param el - the element that is being added to the refs array
+     */
+    const addToRef = (el)=>{
+        if(el && !lineRefs.current.includes(el)){
+            lineRefs.current.push(el)
+        }
+    }  
+    
+    /* It's setting the new content to the content of the block that is active. */
+    useEffect(()=>{
+        setNewContent(content[blockActive -1])
+        setOldContent(content[blockActive -1])
+    }, [])
 
     /**
      * It takes a string, and returns the same string with the first letter capitalized
@@ -57,15 +88,9 @@ const Content = (props) => {
      * @param word - The word that you want to change into an array.
      * @returns An array of the letters of the word
      */
-    const changeTheWordAnArray = (word)=>{
+    const changeAllLattersInTheWordAnArray = (word)=>{
         return word.split("")
     }
-
-    const addToRef = (el)=>{
-        if(el && !lineRefs.current.includes(el)){
-            lineRefs.current.push(el)
-        }
-    }  
 
     /**
      * It takes a word and a color as parameters, and returns a div with a span for each letter of the
@@ -78,8 +103,8 @@ const Content = (props) => {
         return(
             <>
                 {
-                    changeTheWordAnArray(word).map((letter, i) =>
-                        <div ref={addToRef} key={letter} className={"letterTitleContainer " + letter}>
+                    changeAllLattersInTheWordAnArray(word).map((letter, i) =>
+                        <div ref={addToRef} key={i.toString()} className={"letterTitleContainer " + letter}>
                             <span style={{color:color}}>{letter}</span>
                         </div>    
                     )
@@ -88,12 +113,18 @@ const Content = (props) => {
         )
     }
 
+    /**
+     * It takes a content object as an argument and returns a div with the title, city, and text of the
+     * content object
+     * @param content - the content of the current slide
+     * @returns a JSX element.
+     */
     const showContent = (content)=>{
         return(
             <>
                 <div className="titleContainer">
                     {
-                        changingAllLettersOfAWordToSpanInDiv(changeTheFirstLettersToUppercase(content.title), content.textColor, changeTheFirstLettersToUppercase(content.title))
+                        oldContent !== null ? changingAllLettersOfAWordToSpanInDiv(changeTheFirstLettersToUppercase(oldContent.title), oldContent.textColor) : null
                     }
                 </div>
                 <div className="cityContainer">
