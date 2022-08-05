24965cae59a0c80ab78f24a7e094852e7839ebd7
Nicolas <casteranicolas.contact@gmail.com>
2022-08-05 14:56:41 +0200

    Update Content.js

--- src/components/allProjects/components/Content.js
+++ src/components/allProjects/components/Content.js
@@ -4,14 +4,29 @@ import "./_content.scss"
 import { gsap } from "gsap";
 
 const Content = (props) => {
-    //Gsap
+    const content = props.content;
+    const blockActive = props.active
+    
+    const [oldContent, setOldContent] = useState(content[blockActive -1])
+    const [newContent, setNewContent] = useState(content[blockActive -1])
+    
+    const [counter, setCounter] = useState(0)
+    
     const tl = useRef()
     
     const refCity = useRef(null)
+    const newRefCity = useRef(null)
     const refParams = useRef(null)
     
     const lineRefs = useRef([]);
     lineRefs.current = []
+    
+    const newLineRefs = useRef([]);
+    newLineRefs.current = []
+    
+    let arrayLength;
+    
+    //Gsap
 
     /* Creating a timeline that will animate the city and params. */
     useEffect(()=>{
@@ -21,7 +36,7 @@ const Content = (props) => {
                 ease: "Power4.inOut"
             }
         })
-        .fromTo(refCity.current, {y: 200}, {y:0, duration: 1})
+        .fromTo(refCity.current, {y: 200}, {y:0, duration: 1, opacity:1})
         .fromTo(refParams.current, {x: -500}, {x:0},"-=0.1")
     },[])
 
@@ -30,6 +45,10 @@ const Content = (props) => {
         updateLineRefs()
     }, [])
     
+    /**
+     * It's a function that uses the gsap library to animate the lines of text in the about section of
+     * the website
+     */
     const updateLineRefs = () => {
         setTimeout(()=>{
             const timeline = gsap.timeline({
@@ -39,24 +58,54 @@ const Content = (props) => {
             })
             lineRefs.current.map((el, i )=> {
                 if(i === 0){
-                    timeline.fromTo(el, {opacity:1, y: 300}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut", delay: 0.5})
+                    timeline.fromTo(el, {opacity:0, y: 600}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut", opacity: 1})
                 }else{
-                    timeline.fromTo(el, {opacity:1, y: 300}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut"},"-=0.16")
+                    timeline.fromTo(el, {opacity:0, y: 600}, {autoAlpha:1, y:0, duration: 0.2, ease: "Power4.inOut", opacity: 1},"-=0.16")
                 }
             })
         }, 100)
     }
+
+    /**
+     * It takes the current array of refs, maps over them, and animates them to the top of the screen
+     */
+    const updateTwoLineRefs = ()=>{
+        const timeline = gsap.timeline({
+            default:{
+                ease: "Power4.inOut"
+            }
+        })
+        lineRefs.current.map((el, i )=> {
+            if(i === 0){
+                timeline.fromTo(el, {y: 0, opacity: 1}, {autoAlpha:1, y:-300, duration:0.2, ease: "Power4.inOut", opacity: 1 })
+            }else{
+                timeline.fromTo(el, {y: 0, opacity: 1}, {autoAlpha:1, y:-300, duration: 0.2,  ease: "Power4.inOut", opacity: 1},"-=0.16")
+            }
+        })
+    }
+
+    const updateCityStyle = () =>{
+        setTimeout(()=>{
+            const timeline = gsap.timeline({
+                default:{
+                    ease: "Power4.inOut"
+                }
+            })
+            timeline.fromTo(refCity.current, {},{opacity: 0, duration: 0.3, ease: "Power4.inOut"})
+        }, 300)
+    }
+    
+    const secondUpdateCityStyle = () =>{
+        const timeline = gsap.timeline({
+            default:{
+                ease: "Power4.inOut"
+            }
+        })
+        timeline.fromTo(newRefCity.current, {opacity: 0, y: 300},{opacity: 1, y:0, duration: 1, ease: "Power4.inOut"})
+    }
     
     //component
-    const content = props.content;
-    const blockActive = props.active
-    
-    const [oldContent, setOldContent] = useState(null)
-    const [newContent, setNewContent] = useState(null)
 
-    const [counter, setCounter] = useState(0)
-
-    
     /**
      * It takes an element as an argument and adds it to the lineRefs array if it's not already in
      * there
@@ -66,13 +115,32 @@ const Content = (props) => {
         if(el && !lineRefs.current.includes(el)){
             lineRefs.current.push(el)
         }
+    } 
+
+    const addToRefTwo = (el)=>{
+        if(el && !newLineRefs.current.includes(el)){
+            newLineRefs.current.push(el)
+        }
     }  
-    
+
     /* It's setting the new content to the content of the block that is active. */
     useEffect(()=>{
-        setNewContent(content[blockActive -1])
-        setOldContent(content[blockActive -1])
-    }, [])
+        managmentContent()
+    }, [blockActive])
+
+    const managmentContent = async ()=>{
+        await setCounter( counter + 1)
+        if(counter > 0){
+            await setNewContent(content[blockActive -1])
+            await updateTwoLineRefs()
+            await updateCityStyle()
+            await setTimeout(()=>{
+                setOldContent(content[blockActive -1])
+                updateLineRefs()
+                secondUpdateCityStyle()
+            }, 300)
+        }    
+    }
 
     /**
      * It takes a string, and returns the same string with the first letter capitalized
@@ -89,8 +157,11 @@ const Content = (props) => {
      * @returns An array of the letters of the word
      */
     const changeAllLattersInTheWordAnArray = (word)=>{
-        return word.split("")
+        word = word.split("")
+        arrayLength = word.length
+        return word
     }
+    
 
     /**
      * It takes a word and a color as parameters, and returns a div with a span for each letter of the
@@ -100,17 +171,67 @@ const Content = (props) => {
      * @returns A div with a span inside of it.
      */
     const changingAllLettersOfAWordToSpanInDiv = (word, color)=>{
-        return(
-            <>
-                {
-                    changeAllLattersInTheWordAnArray(word).map((letter, i) =>
-                        <div ref={addToRef} key={i.toString()} className={"letterTitleContainer " + letter}>
-                            <span style={{color:color}}>{letter}</span>
-                        </div>    
-                    )
-                }
-            </>
-        )
+        if(counter <= 1){
+            return(
+                <>
+                    <div className="thirsTitleContainer">
+                        {
+                            changeAllLattersInTheWordAnArray(word).map((letter, i) =>
+                                <div ref={addToRef} key={i.toString()} className={"letterTitleContainer " + letter}>
+                                    <span style={{color:color}}>{letter}</span>
+                                </div>    
+                            )
+                        }
+                    </div>
+                </>
+            )
+        }else{
+            return(
+                <>
+                    <div className="thirsTitleContainer">
+                        {
+                            changeAllLattersInTheWordAnArray(changeTheFirstLettersToUppercase(oldContent.title)).map((letter, i) =>
+                                <div ref={addToRef} key={i.toString()} className={"letterTitleContainer " + letter}>
+                                    <span style={{color:color}}>{letter}</span>
+                                </div>    
+                            )
+                        }
+                    </div>
+                    <div className="secondTitleContainer">
+                        {
+                            changeAllLattersInTheWordAnArray(changeTheFirstLettersToUppercase(newContent.title)).map((letter, i) =>
+                            <div ref={addToRefTwo} key={i.toString() + 2} className={"letterTitleContainer " + letter}>
+                                <span style={{color:color}}>{letter}</span>
+                            </div>    
+                            )
+                        }
+                    </div>
+                </>
+            )
+        }
+    }
+
+    const updateCity = (content)=>{
+        if(counter <= 1){
+            return(
+                <>
+                    <div className="thirsCityContainer">
+                        <h4 ref={refCity} className='city'>{changeTheFirstLettersToUppercase(content.city)}, <span className='date'>{changeTheFirstLettersToUppercase(content.years)}</span></h4>
+                    </div>
+                </>
+            )
+        }else{
+            return(
+                <>
+                    <div className="thirsCityContainer">
+                            <h4 ref={refCity} className='city'>{changeTheFirstLettersToUppercase(oldContent.city)}, <span className='date'>{changeTheFirstLettersToUppercase(oldContent.years)}</span></h4>
+                    </div>
+                    <div className="secondCityContainer">
+                            <h4 ref={newRefCity} className='city'>{changeTheFirstLettersToUppercase(newContent.city)}, <span className='date'>{changeTheFirstLettersToUppercase(newContent.years)}</span></h4>
+                    </div>
+                </>
+            )
+        }
     }
 
     /**
@@ -124,11 +245,14 @@ const Content = (props) => {
             <>
                 <div className="titleContainer">
                     {
-                        oldContent !== null ? changingAllLettersOfAWordToSpanInDiv(changeTheFirstLettersToUppercase(oldContent.title), oldContent.textColor) : null
+                        content !== null ? changingAllLettersOfAWordToSpanInDiv(changeTheFirstLettersToUppercase(content.title), (content.textColor)) : null
                     }
                 </div>
                 <div className="cityContainer">
-                    <h4 ref={refCity} className='city'>{changeTheFirstLettersToUppercase(content.city)}, <span className='date'>{changeTheFirstLettersToUppercase(content.years)}</span></h4>
+                    {
+                        content !== null ? updateCity(content): null
+                    }
+                     {/* <h4 ref={refCity} className='city'>{changeTheFirstLettersToUppercase(content.city)}, <span className='date'>{changeTheFirstLettersToUppercase(content.years)}</span></h4> */}
                 </div>
                 <div className="paramsContainer">
                     <p ref={refParams} className='params'>{content.text}</p>
