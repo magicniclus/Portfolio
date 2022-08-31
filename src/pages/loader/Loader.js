8449f488fbb1f8c05f83a13a95e9f2d3ab951566
Nicolas <casteranicolas.contact@gmail.com>
2022-08-31 11:43:01 +0200

    Update Loader.js

--- src/pages/loader/Loader.js
+++ src/pages/loader/Loader.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import BackgroudSVG from './component/BackgroudSVG';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import "./_loader.scss";
-import { cursorDifferenceLeave, cursorDifferenceOver } from '../../redux/actions/actions';
 
 const Loader = () => {
     const dispatch = useDispatch()
 
-    const changeCursorOver = ()=>{
-        dispatch(cursorDifferenceOver())
-    }
-
-    const changeCursorLeave = ()=>{
-        dispatch(cursorDifferenceLeave())
-        console.log('leave');
-    }
-
     return (
-        <div className='loader' onMouseEnter={changeCursorOver} onMouseOut={changeCursorLeave}>
+        <div className='loader'>
             <BackgroudSVG />
             <div className="topContainer">
                 <div className="number">
