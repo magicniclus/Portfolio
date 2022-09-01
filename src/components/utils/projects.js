bf904ad3f93e3b04ef5bfdef00921cc21dd67a1d
Nicolas <casteranicolas.contact@gmail.com>
2022-09-01 17:06:09 +0200

    Update projects.js

--- src/components/utils/projects.js
+++ src/components/utils/projects.js
@@ -7,8 +7,8 @@ export let projects = [
         text: "TeraBois, spécialiste en économie d'énergie de l'habitat depuis plus de 10 ans.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
         img: "one.jpg",
-        color: "RGBA(186,165,179,0.1)",
-        textColor: "#4194A3",
+        color: "RGBA(106,76,32,0.1)",
+        textColor: "#4b6a6d",
         link: "https://www.terabois-isolation.fr/"
     },
     {
@@ -16,11 +16,11 @@ export let projects = [
         title: "Petits Plats",
         city: "Bordeaux",
         years: "2022",
-        text: "Les Petits Plats est un projet d'école qui demandait de réaliser en javascript un alogrithe de recherche.",
+        text: "Les Petits Plats est un projet d'école qui demandait de réaliser en javascript un alogrithme de recherche.",
         skills: ["Javascript", "Sass"],
         img: "two.jpg",
-        color: "RGBA(147,163,168,0.1)",
-        textColor: "#435c75",
+        color: "RGBA(32,28,51,0.1)",
+        textColor: "#9e4448",
         link: "https://magicniclus.github.io/LesPetitsPlats/"
     },
     {
@@ -30,9 +30,9 @@ export let projects = [
         years: "2020",
         text: "Fish Eye est une plateforme de photographe leurs permettant de montrer leurs projets à leurs clients.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
-        img: "four.jpg",
-        color: "RGBA(98,58,51,0.1)",
-        textColor: "#333132",
+        img: "three.jpg",
+        color: "RGBA(253,236,200,0.2)",
+        textColor: "#6e361d",
         link: "https://magicniclus.github.io/NicolasCastera_6_06052021/"
     },
     {
@@ -42,9 +42,9 @@ export let projects = [
         years: "2022",
         text: "Kaza est un projet d'école reprenant l'ensemble des bases de React et pas de Sass.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
-        img: "three.jpg",
-        color: "RGBA(200,193,189,0.1)",
-        textColor: "#517295",
+        img: "four.jpg",
+        color: "RGBA(183,126,25,0.1)",
+        textColor: "#0d4671",
         link: "https://github.com/magicniclus/magicniclus-NicolasCastera_11_06052021"
     }
 ]
