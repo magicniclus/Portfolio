64855399eee5db29e1c63d669a2b6e3edfc6aed3
Nicolas <casteranicolas.contact@gmail.com>
2022-09-01 15:27:27 +0200

    Update projects.js

--- src/components/utils/projects.js
+++ src/components/utils/projects.js
@@ -4,44 +4,48 @@ export let projects = [
         title: "terabois",
         city: "bordeaux",
         years: "2022",
-        text: "Terabois ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        text: "TeraBois, spécialiste en économie d'énergie de l'habitat depuis plus de 10 ans.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
         img: "one.jpg",
-        color: "RGBA(186,165,179,0.2)",
-        textColor: "#4194A3"
+        color: "RGBA(186,165,179,0.1)",
+        textColor: "#4194A3",
+        link: "https://www.terabois-isolation.fr/"
     },
     {
         id: 2,
-        title: "maison-sûr",
-        city: "Paris",
-        years: "2017",
-        text: "Maison-sur ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
-        skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
+        title: "Petits Plats",
+        city: "Bordeaux",
+        years: "2022",
+        text: "Les Petits Plats est un projet d'école qui demandait de réaliser en javascript un alogrithe de recherche.",
+        skills: ["Javascript", "Sass"],
         img: "two.jpg",
-        color: "RGBA(147,163,168,0.2)",
-        textColor: "#435c75"
+        color: "RGBA(147,163,168,0.1)",
+        textColor: "#435c75",
+        link: "https://magicniclus.github.io/LesPetitsPlats/"
     },
     {
         id: 3,
-        title: "magma",
-        city: "Paris",
-        years: "2019",
-        text: "Magma ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        title: "Fish Eye",
+        city: "Bordeaux",
+        years: "2020",
+        text: "Fish Eye est une plateforme de photographe leurs permettant de montrer leurs projets à leurs clients.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
-        img: "three.jpg",
-        color: "RGBA(200,193,189,0.2)",
-        textColor: "#517295"
+        img: "four.jpg",
+        color: "RGBA(98,58,51,0.1)",
+        textColor: "#333132",
+        link: "https://magicniclus.github.io/NicolasCastera_6_06052021/"
     },
     {
         id: 4,
-        title: "three-js",
+        title: "Kaza",
         city: "Bordeaux",
-        years: "2020",
-        text: "Three-js ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim.",
+        years: "2022",
+        text: "Kaza est un projet d'école reprenant l'ensemble des bases de React et pas de Sass.",
         skills: ["React", "Sass", "Redux", "GSAP", "Strapi"],
-        img: "four.jpg",
-        color: "RGBA(98,58,51,0.2)",
-        textColor: "#333132"
+        img: "three.jpg",
+        color: "RGBA(200,193,189,0.1)",
+        textColor: "#517295",
+        link: "https://github.com/magicniclus/magicniclus-NicolasCastera_11_06052021"
     }
 ]
 
