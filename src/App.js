633eda0c3f19b9c6a51965396de9abaf807dfd05
Nicolas <casteranicolas.contact@gmail.com>
2022-09-05 11:54:49 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,90 @@ import TestPage from "./pages/testPage/TestPage";
 import "./styles/global.scss"
 import Loader from "./pages/loader/Loader";
 import { useDispatch, useSelector } from 'react-redux';
+import { changeColor, changeTextColor, projectIsClose, updateScreenSize } from "./redux/actions/actions";
+import MobilPage from "./pages/mobilPage/MobilPage";
+import { homePageLoading } from './redux/actions/actions';
+
 
 function App() {
-
+  
   let cursor = useRef()
-
+  
   const isLoading = useSelector(state => state.isLoading)
   const projectIsOpen = useSelector(state => state.projectIsOpen)
-
+  
   const cursorUpdate = useSelector(state=>state.cursorDifference)
 
+  const mobilSize = useSelector(state => state.mobilSize)
+  
   const [whatPage, setWhatPage] = useState(<Loader />)
 
+  const dispatch = useDispatch()
+
   useEffect(()=>{
-    if(isLoading)setWhatPage(<HomePage />)
-  }, [isLoading])
+      window.addEventListener('load', (event) => {
+          setTimeout(()=>{
+              dispatch(homePageLoading())
+              window.scrollTo(0, 0);
+          }, 5000)
+      });
+  }, [])
+  
+  const useIsMobile = () => {
+    useEffect(() => {
+        const onResize = () => {
+            if(window.innerWidth <= 1024){
+              dispatch(updateScreenSize(true))
+            }else{
+              dispatch(updateScreenSize(false))
+
+            }
+        }
+
+        window.addEventListener("resize", onResize);
+    
+        return () => {
+            window.removeEventListener("resize", onResize);
+        }
+    }, []);
+  }
+ 
+  useIsMobile()
+
+  useEffect(() => {
+          if(window.innerWidth <= 1024){
+            dispatch(updateScreenSize(true))
+          }else{
+            dispatch(updateScreenSize(false))
+          }
+  }, []);
+    
+  useEffect(()=>{
+    if(window.innerWidth <= 1024){
+      dispatch(projectIsClose());
+      dispatch(changeColor("#EDEAE6"));
+      dispatch(changeTextColor("#373634"))
+    }
+
+
+    if(!mobilSize && !isLoading){
+        setWhatPage(<HomePage />)
+    }else if (!mobilSize && isLoading){
+      setWhatPage(<Loader />)
+    }else {
+      setWhatPage(<MobilPage />)
+    }
+  }, [mobilSize])
+
+  useEffect(()=>{
+    if(!mobilSize && !isLoading){
+        setWhatPage(<HomePage />)
+    }else if (!mobilSize && isLoading){
+      setWhatPage(<Loader />)
+    }else {
+      setWhatPage(<MobilPage />)
+    }
+  }, [isLoading]) 
 
   return (
     <div  className="app" style={projectIsOpen || isLoading ? {height: '100vh', overflow: "hidden"} : {height: '100%', overflow: "auto"}}>
