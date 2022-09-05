409467a31c3f19e9c6e22a28636d9478f5bcb0a4
Nicolas <casteranicolas.contact@gmail.com>
2022-09-05 11:55:05 +0200

    Create MobilPage.js

---
+++ src/pages/mobilPage/MobilPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MobilPage = () => {
+    return (
+        <>
+          <header className='mobilHeader'>MobilPage</header>  
+          <main className='mobilMain'>
+            <h1>Hello Word</h1>
+          </main>
+        </>
+    );
+};
+
+export default MobilPage;
