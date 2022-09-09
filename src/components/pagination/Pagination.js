6d548a23c498d1617a763aa94003a73f17d85012
Nicolas <casteranicolas.contact@gmail.com>
2022-09-09 18:28:39 +0200

    Create Pagination.js

---
+++ src/components/pagination/Pagination.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./_pagination.scss"
+
+const Pagination = () => {
+    return (
+        <div className='pagineation'>
+            
+        </div>
+    );
+};
+
+export default Pagination;
